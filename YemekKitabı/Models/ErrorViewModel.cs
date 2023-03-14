90cd765c1973c2b9c9f7125b09dc66d7a15dc4a1
bernabayraktar <bernabayraktar24@gmail.com>
2023-03-14 11:35:12 +0300

    Project created

---
+++ YemekKitabı/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YemekKitabı.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
