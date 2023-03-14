78895c6929030b251fb4086348d63fd48fe1d69b
BerkeErd <39529687+BerkeErd@users.noreply.github.com>
2023-03-14 11:48:41 +0300

    added Connection String

---
+++ YemekKitabı/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace YemekKitabı.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {
+            
+        }
+    }
+}
